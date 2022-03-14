458b44b4c3abb4110b69bc4c1cd0bfda7d1cedb3
iwakitakuma33 <iwakitakuma33@gmail.com>
2022-03-14 18:30:04 +0900

    【追加】GraphComponent

---
+++ src/components/Graph.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Graph: React.FC = () => {
+  return <div>graph</div>;
+};
+
+export default Graph;
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import CheckBoxField from "./CheckBoxField";
+import Graph from "./Graph";
 
 const Main: React.FC = () => {
   return (
     <main>
       <div>main</div>
       <CheckBoxField />
+      <Graph />
     </main>
   );
 };
