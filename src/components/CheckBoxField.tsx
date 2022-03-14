788c395e4598c79550e94fb3a9e154ee92c809ec
iwakitakuma33 <iwakitakuma33@gmail.com>
2022-03-14 18:10:04 +0900

    【追加】CheckBoxFieldComponent

---
+++ src/components/CheckBoxField.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CheckBoxField: React.FC = () => {
+  return <>checkbox</>;
+};
+
+export default CheckBoxField;
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import CheckBoxField from "./CheckBoxField";
 
 const Main: React.FC = () => {
   return (
     <main>
       <div>main</div>
+      <CheckBoxField />
     </main>
   );
 };
