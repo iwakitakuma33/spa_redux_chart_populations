a307ffdc5deba14db4f7dc1161a0191fc8d5d0bd
iwakitakuma33 <iwakitakuma33@gmail.com>
2022-03-14 18:30:04 +0900

    【追加】MainComponent

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
 import React from "react";
+import Main from "./components/Main";
+import { contents } from "./constants/contents";
+
 const App: React.FC = () => {
-  return <div className="App">HelloWorld</div>;
+  return (
+    <div className="App">
+      <header style={{ textAlign: "center" }}>
+        <h1>{contents.title}</h1>
+      </header>
+      <Main />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/Main.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Main: React.FC = () => {
+  return (
+    <main>
+      <div>main</div>
+    </main>
+  );
+};
+
+export default Main;
