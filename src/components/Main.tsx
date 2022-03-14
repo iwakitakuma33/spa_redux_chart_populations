8a35d62f7b9575f2c5ff4cddbc8b733f0840a224
iwakitakuma33 <iwakitakuma33@gmail.com>
2022-03-14 23:59:07 +0900

    Mainコンポーネント内で都道府県を取得 prefectures storeを更新

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,15 +1,23 @@
 import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
 import CheckBoxField from "./CheckBoxField";
 import Graph from "./Graph";
 
+import { fetchPrefs } from "../store/prefectures/action";
+import { selectPrefs } from "../store/prefectures/selectore";
+
 import { apiFetcher } from "../utils/apiFetcher/index";
 import { apiConfig } from "../constants/apiConfig";
+import { contents } from "../constants/contents";
 
 const Main: React.FC = () => {
+  const dispatch = useDispatch();
+  const prefectures = useSelector(selectPrefs);
   useEffect(() => {
     const apiEndpointPrefsALL = apiConfig.endpoints.prefectures.all;
     apiFetcher.get(apiEndpointPrefsALL).then((results) => {
-      console.log(results);
+      dispatch(fetchPrefs(results.data));
     });
   }, []);
 
