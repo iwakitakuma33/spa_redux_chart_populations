0b3d812a90c61abd7a9ec2d256296a4aee4942ac
iwakitakuma33 <iwakitakuma33@gmail.com>
2022-03-15 21:45:17 +0900

    [更新]GraphComponent サンプルチャートを表示

--- src/components/Graph.tsx
+++ src/components/Graph.tsx
@@ -1,7 +1,39 @@
 import React from "react";
+import Highcharts from "highcharts";
+import HighchartsReact from "highcharts-react-official";
+
+import { graphConfig } from "../constants/graphConfig";
 
 const Graph: React.FC = () => {
-  return <div>graph</div>;
+  const options: Highcharts.Options = {
+    title: {
+      text: graphConfig.graphLabel.title,
+    },
+    xAxis: {
+      title: {
+        text: graphConfig.graphLabel.xLabel,
+      },
+      categories: ["1", "2", "3"],
+    },
+    yAxis: {
+      title: {
+        text: graphConfig.graphLabel.yLabel,
+      },
+    },
+    series: [],
+  };
+
+  return (
+    <div style={Styles.graph}>
+      <HighchartsReact highcharts={Highcharts} options={options} />
+    </div>
+  );
+};
+
+const Styles: { [key: string]: React.CSSProperties } = {
+  graph: {
+    padding: "12px",
+  },
 };
 
 export default Graph;
